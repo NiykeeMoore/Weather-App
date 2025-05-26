157475553b15bae989d5eef7171851049f30ce15
Vladimir Golovin <cocacolalocal@ya.ru>
2025-05-26 21:01:57 +0300

    initial commit

---
+++ Weather App/Weather App/Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Weather_AppApp.swift
+//  Weather App
+//
+//  Created by Niykee Moore on 26.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
